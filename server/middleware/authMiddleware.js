bdd8001aa4a2e818d0c363b851bf6b37416fe04e
rahulll-rai <pr9068124@gmail.com>
2026-05-10 14:51:06 +0530

    Fix token verification on vercel by providing fallback JWT_SECRET

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -10,7 +10,7 @@ const protect = async (req, res, next) => {
   ) {
     try {
       token = req.headers.authorization.split(" ")[1];
-      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+      const decoded = jwt.verify(token, process.env.JWT_SECRET || "mysecretkey");
       req.user = await User.findById(decoded.id).select("-password");
       next();
     } catch (error) {
